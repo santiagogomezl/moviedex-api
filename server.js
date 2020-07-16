919a48235e65a083ca685ad3e88853827cfe61e1
Santiago Gomez <santiagogomezl@santiagos-mbp.attlocal.net>
2020-07-15 19:09:03 -0700

    moviedex-api v1

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
 const MOVIEDEX = require('./movies-data-small.json')
 
 const app = express()
+
 app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
+
+app.use(function validateBearerToken(req, res, next){
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+    
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+      }
+
+    next()
+})
 
 app.get('/movie', function handleGetMovie(req, res){
     let response = MOVIEDEX;
