1fca6616529d006d560105238b805661867bedd8
Santiago Gomez <santiagogomezl@santiagos-mbp.attlocal.net>
2020-07-16 19:27:32 -0700

    moviedex-api production

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const MOVIEDEX = require('./movies-data-small.json')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morgarSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morgarSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -46,6 +47,15 @@ app.get('/movie', function handleGetMovie(req, res){
     res.json(response)
 })
 
-app.listen(8000, ()=>{
-    console.log('Server listening to port 8000')
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
 })
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, ()=>{})
